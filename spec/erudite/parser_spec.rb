ae68cb2bc75725e6e9773a6fec787493408b0140
Taylor Fausak <taylor@fausak.me>
2014-08-24 23:25:30 -0500

    Improve spec layout

--- spec/erudite/parser_spec.rb
+++ spec/erudite/parser_spec.rb
@@ -2,67 +2,74 @@
 
 require 'spec_helper'
 
+# Monkey patch String to add ability to strip leading whitespace.
+class String
+  def dedent
+    gsub(/^#{self[/\A\s*/]}/, '')
+  end
+end
+
 describe Erudite::Parser do
   describe '.parse' do
     it 'parses an example without output or a result' do
-      examples = described_class.parse(<<-'RUBY')
->> :something
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> :something
       RUBY
       expect(examples).to eq([
         Erudite::Example.new(':something')])
     end
 
     it 'parses an example with a result' do
-      examples = described_class.parse(<<-'RUBY')
->> :something
-=> :something
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> :something
+        => :something
       RUBY
       expect(examples).to eq([
         Erudite::Example.new(':something', ':something')])
     end
 
     it 'parses an example with output' do
-      examples = described_class.parse(<<-'RUBY')
->> puts :something
-something
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> puts :something
+        something
       RUBY
       expect(examples).to eq([
         Erudite::Example.new('puts :something', nil, 'something')])
     end
 
     it 'handles STDIN' do
-      examples = described_class.parse(<<-'RUBY')
->> gets
-=> nil
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> gets
+        => nil
       RUBY
       expect(examples).to eq([
         Erudite::Example.new('gets', 'nil')])
     end
 
     it 'handles STDOUT' do
-      examples = described_class.parse(<<-'RUBY')
->> puts :something
-something
-=> nil
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> puts :something
+        something
+        => nil
       RUBY
       expect(examples).to eq([
         Erudite::Example.new('puts :something', 'nil', 'something')])
     end
 
     it 'handles STDERR' do
-      examples = described_class.parse(<<-'RUBY')
->> warn :something
-something
-=> nil
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> warn :something
+        something
+        => nil
       RUBY
       expect(examples).to eq([
         Erudite::Example.new('warn :something', 'nil', 'something')])
     end
 
     it 'handles exceptions' do
-      examples = described_class.parse(<<-'RUBY')
->> fail 'something'
-RuntimeError: something
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> fail 'something'
+        RuntimeError: something
       RUBY
       expect(examples).to eq([
         Erudite::Example.new(
@@ -70,24 +77,24 @@ RuntimeError: something
     end
 
     it 'handles multi-line source and output' do
-      examples = described_class.parse(<<-'RUBY')
->> puts 'some
-.. thing'
-some
-thing
-=> nil
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> puts 'some
+        .. thing'
+        some
+        thing
+        => nil
       RUBY
       expect(examples).to eq([
         Erudite::Example.new("puts 'some\nthing'", 'nil', "some\nthing")])
     end
 
     it 'handles multiple examples' do
-      examples = described_class.parse(<<-'RUBY')
->> def some
-..   :thing
-.. end
->> some
-=> :thing
+      examples = described_class.parse(<<-'RUBY'.dedent)
+        >> def some
+        ..   :thing
+        .. end
+        >> some
+        => :thing
       RUBY
       expect(examples).to eq([
         Erudite::Example.new("def some\n  :thing\nend"),
