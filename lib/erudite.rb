f6b0ac9aab60ee9117e8bcc2fa5159ef956f642a
Taylor Fausak <taylor@fausak.me>
2015-01-22 09:35:21 -0600

    Create Erudite::VERSION

--- lib/erudite.rb
+++ lib/erudite.rb
@@ -4,6 +4,7 @@ require 'erudite/example'
 require 'erudite/executable'
 require 'erudite/outcome'
 require 'erudite/parser'
+require 'erudite/version'
 
 # Executable documentation.
 module Erudite
---
+++ lib/erudite/version.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+module Erudite # rubocop:disable Style/Documentation
+  VERSION = Gem::Version.new('0.2.0')
+end
