46b5b884c9420144fa869aec004cb7eea277164d
Taylor Fausak <taylor@fausak.me>
2015-01-22 22:36:05 -0600

    Define the module before requiring other files

--- lib/erudite.rb
+++ lib/erudite.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 
+# Executable documentation.
+module Erudite
+end
+
 require 'erudite/example'
 require 'erudite/executable'
 require 'erudite/outcome'
 require 'erudite/parser'
 require 'erudite/version'
-
-# Executable documentation.
-module Erudite
-end
