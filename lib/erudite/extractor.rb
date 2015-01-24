e6f8f2252708283e98c31bb130e68f50815b22f8
Taylor Fausak <taylor@fausak.me>
2015-01-23 18:32:14 -0600

    Add a function for parsing comments

--- lib/erudite/extractor.rb
+++ lib/erudite/extractor.rb
@@ -1,7 +1,12 @@
 # coding: utf-8
 
+require 'parser/current'
+
 module Erudite
   # Extracts examples from comments.
   class Extractor
+    def self.parse_comments(source)
+      Parser::CurrentRuby.parse_with_comments(source).last
+    end
   end
 end
--- spec/erudite/extractor_spec.rb
+++ spec/erudite/extractor_spec.rb
@@ -3,4 +3,21 @@
 require 'spec_helper'
 
 describe Erudite::Extractor do
+  describe '.parse_comments' do
+    subject(:result) { described_class.parse_comments(source) }
+    let(:source) { '' }
+
+    it 'returns an array' do
+      expect(result).to be_an(Array)
+    end
+
+    context 'with a comment' do
+      let(:source) { '# Call me Ishmael.' }
+
+      it 'returns the comment' do
+        expect(result).to_not be_empty
+        result.each { |e| expect(e).to be_a(Parser::Source::Comment) }
+      end
+    end
+  end
 end
